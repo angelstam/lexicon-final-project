3763a72114a84d8c8682a4e497a269c127996e46
Johan Angelstam <johan@angelstam.se>
2024-10-10 10:42:16 +0200

    Add styling to CarDetail.

--- src/pages/car/CarDetail.tsx
+++ src/pages/car/CarDetail.tsx
@@ -4,6 +4,7 @@ import ReturnButton from "../../shared/ReturnButton";
 import { Vehicle } from "../../shared/data/Vehicle";
 import { useParams } from "react-router-dom";
 import * as Vehicles from "../../shared/data/Vehicles";
+import "./CarDetail.css";
 
 export default function CarDetail(): ReactNode {
   const [vehicle, setVehicle] = useState<Vehicle>();
@@ -16,14 +17,16 @@ export default function CarDetail(): ReactNode {
   }, []);
 
   return (
-    <article className="car-detail">
+    <>
       <Header><ReturnButton /></Header>
-      <h2>{vehicle?.make} {vehicle?.model}</h2>
-      <figure><img src={vehicle?.photo} alt="Photo of car" /></figure>
-      <section>
-        <p>{vehicle?.odometer} km</p>
-        <p>{vehicle?.fuelConsumption} l / 10 km</p>
-      </section>
-    </article>
+      <article className="car-detail">
+        <h2>{vehicle?.make} {vehicle?.model}</h2>
+        <figure><img src={vehicle?.photo} alt="Photo of car" /></figure>
+        <section>
+          <p>{vehicle?.odometer} km</p>
+          <p>{vehicle?.fuelConsumption} l / 10 km</p>
+        </section>
+      </article>
+    </>
   );
 }
