96636a987fe5ef47fa1b9514927836a077b19fe9
Johan Angelstam <johan@angelstam.se>
2024-10-10 10:32:21 +0200

    Add styling to CarListItem.

--- src/pages/car/CarListItem.tsx
+++ src/pages/car/CarListItem.tsx
@@ -1,20 +1,18 @@
 import { ReactNode } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Vehicle } from "../../shared/data/Vehicle";
+import "./CarListItem.css";
 
 export default function CarListItem({ vehicle }: { vehicle: Vehicle }): ReactNode {
 
-  // Hook to handle navigation
-  const navigate = useNavigate();
-
   return (
-    <article className="car-list-item" onClick={() => navigate(`/car/${vehicle.id}`)}>
+    <Link className="car-list-item" to={`/car/${vehicle.id}`}>
       <figure><img src={vehicle.photo} alt="Photo of car" /></figure>
       <section>
         <h3>{vehicle?.make} {vehicle?.model}</h3>
         <p>{vehicle?.odometer} km</p>
         <p>{vehicle?.fuelConsumption} l / 10 km</p>
       </section>
-    </article>
+    </Link>
   );
 }
